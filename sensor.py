6ae01b6f773a0ad9d00f929cd374a56c53064c3e
zrincet <zrincet@gmail.com>
2020-07-31 23:37:44 +0800

    Update sensor.py
    to v1.1

--- sensor.py
+++ sensor.py
@@ -30,14 +30,16 @@ COMPONENT_REPO = 'https://github.com/zrincet/IHC8342B_query/'
 SCAN_INTERVAL = timedelta(seconds=300)
 CONF_OPTIONS = "options"
 CONF_MAC = "mac"
+CONF_PRICE = "price"
 
 ATTR_NAME = "名称"
 ATTR_UPDATE_TIME = "更新时间"
 ATTR_DATA_TIME = "数据时间"
 ATTR_MAC_ADDR = "MAC地址"
+ATTR_PRICE = "电费"
 
 OPTIONS = dict(eleTotal=["IHC8342B_ele_total", "累计用电", "mdi:flash", "kW·h", "fw_hongyanelec_v1"],
-               eleHour=["IHC8342B_ele_hour", "小时用电", "mdi:flash", "kW·h", "fw_hydayelec_v1"],
+               # eleHour=["IHC8342B_ele_hour", "小时用电", "mdi:flash", "kW·h", "fw_hydayelec_v1"],  #  no useful
                eleToday=["IHC8342B_ele_today", "今日用电", "mdi:flash", "kW·h", "fw_hongyanelec_v1"],
                eleMonth=["IHC8342B_ele_month", "本月用电", "mdi:flash", "kW·h", "fw_hymonthelec_v1"],
                eleYear=["IHC8342B_ele_year", "年度用电", "mdi:flash", "kW·h", "fw_hyyearelec_v1"],
@@ -46,7 +48,8 @@ OPTIONS = dict(eleTotal=["IHC8342B_ele_total", "累计用电", "mdi:flash", "kW�
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_MAC): cv.string,
     vol.Required(CONF_NAME): cv.string,
-    vol.Required(CONF_OPTIONS, default=[]): vol.All(cv.ensure_list, [vol.In(OPTIONS)]),
+    vol.Required(CONF_PRICE, default=0.53): cv.string,
+    vol.Required(CONF_OPTIONS, default=['eleTotal']): vol.All(cv.ensure_list, [vol.In(OPTIONS)]),
 })
 
 
@@ -55,21 +58,25 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     _LOGGER.info("async_setup_platform sensor IHC8342B Sensor")
     dev = []
     for option in config[CONF_OPTIONS]:
-        dev.append(IHC8342BSensor(config[CONF_MAC], config[CONF_NAME], option))
+        dev.append(IHC8342BSensor(config[CONF_MAC], config[CONF_NAME], config[CONF_PRICE], option))
 
     async_add_devices(dev, True)
 
 
 class IHC8342BSensor(Entity):
-    def __init__(self, mac_user_input, name, option):
+    def __init__(self, mac_user_input, name, price, option):
         self._macUserInput = mac_user_input
         self._state = None
         self._mac = str(mac_user_input).replace(":", "").replace("：", "").lower()
+        self._price = float(price)
 
         self._eleTotal = None
         self._eleToday = None
         self._eleMonth = None
         self._eleYear = None
+
+        self._startTime = None
+        self._endTime = None
 
         self._power = None
         self._dataTime = None
@@ -79,29 +86,45 @@ class IHC8342BSensor(Entity):
         self._friendly_name = str(name) + '_' + OPTIONS[option][1]
         self._icon = OPTIONS[option][2]
         self._unit_of_measurement = OPTIONS[option][3]
+        self._postType = OPTIONS[option][4]
         self._type = option
 
     def update(self):
         import datetime
         import time
+        import calendar
 
         timeNow = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+        monthRange = calendar.monthrange(timeNow.year, timeNow.month)[1]
 
         _LOGGER.info("IHC8342B Sensor start updating data.")
         header = {
             'Content-Type': 'application/json; charset=utf-8',
             'User-Agent': 'okhttp/3.11.0'
         }
-        url = 'https://hongyan.ibroadlink.com/dataservice/v2/device/status'
+        if self._type == "eleTotal" or self._type == "eleToday" or self._type == "power":
+            url = 'https://hongyan.ibroadlink.com/dataservice/v2/device/status'
+            self._startTime = timeNow.strftime("%Y-%m-%d_00:00:00")
+            self._endTime = timeNow.strftime("%Y-%m-%d_23:59:59")
+
+        elif self._type == "eleMonth":
+            url = 'https://hongyan.ibroadlink.com/dataservice/v2/device/stats'
+            self._startTime = timeNow.strftime("%Y-%m-01_00:00:00")
+            self._endTime = timeNow.strftime("%Y-%m-day_23:59:59").replace("day", str(monthRange))
+
+        elif self._type == "eleYear":
+            url = 'https://hongyan.ibroadlink.com/dataservice/v2/device/stats'
+            self._startTime = timeNow.strftime("%Y-01-01_00:00:00")
+            self._endTime = timeNow.strftime("%Y-12-31_23:59:59")
 
         data = {
-            "report": "fw_hongyanelec_v1",
+            "report": self._postType,
             "device": [
                 {
                     "did": "00000000000000000000" + self._mac,
                     "params": ["power", "elec", "occurtime"],
-                    "start": timeNow.strftime("%Y-%m-%d_00:00:00"),
-                    "end": timeNow.strftime("%Y-%m-%d_23:59:59")
+                    "start": self._startTime,
+                    "end": self._endTime
                 }
             ]
         }
@@ -113,19 +136,44 @@ class IHC8342BSensor(Entity):
             _LOGGER.error("Unable to connect to HonYar server. %s", error)
 
         try:
-            self._eleTotal = float(re_json['table'][0]['values'][0]['elec'])/100
-            self._power = float(re_json['table'][0]['values'][0]['power'])/100
-            self._eleToday = float(re_json['table'][0]['values'][0]['elec'])/100 - float(re_json['table'][0]['values'][-1]['elec'])/100
+
             self._dataTime = str(re_json['table'][0]['values'][0]['occurtime']).replace("_", " ")
 
             self._updateTime = timeNow.strftime("%Y-%m-%d %H:%M:%S")
 
             if self._type == "eleTotal":
+                self._eleTotal = float(re_json['table'][0]['values'][0]['elec']) / 100
                 self._state = self._eleTotal
+
             elif self._type == "eleToday":
+                self._eleToday = round(float(re_json['table'][0]['values'][0]['elec']) / 100 - float(
+                    re_json['table'][0]['values'][-1]['elec']) / 100, 2)
                 self._state = self._eleToday
+
             elif self._type == "power":
+                self._power = float(re_json['table'][0]['values'][0]['power']) / 100
                 self._state = self._power
+
+            elif self._type == "eleMonth":
+                if len(re_json['table'][0]['values']) == 1:
+                    self._eleMonth = float(re_json['table'][0]['values'][0]['elec']) / 100
+                else:
+                    self._eleMonth = round(float(re_json['table'][0]['values'][-1]['elec']) / 100 - float(
+                        re_json['table'][0]['values'][0]['elec']) / 100, 2)
+                self._state = self._eleMonth
+                self._dataTime = str(re_json['table'][0]['values'][0]['occurtime']).split("_")[0] + \
+                                 str(re_json['table'][0]['values'][-1]['occurtime']).split("_")[0]
+
+            elif self._type == "eleYear":
+                if len(re_json['table'][0]['values']) == 1:
+                    self._eleYear = float(re_json['table'][0]['values'][0]['elec']) / 100
+                else:
+                    self._eleYear = round(float(re_json['table'][0]['values'][-1]['elec']) / 100 - float(
+                        re_json['table'][0]['values'][0]['elec']) / 100, 2)
+                self._state = self._eleYear
+                self._dataTime = str(re_json['table'][0]['values'][0]['occurtime']).split("_")[0] + str(
+                    re_json['table'][0]['values'][-1]['occurtime']).split("_")[0]
+
         except Exception as e:
             _LOGGER.error("Something wrong in IHC8342B sensor. %s", e.args[0])
 
@@ -140,6 +188,7 @@ class IHC8342BSensor(Entity):
     @property
     def icon(self):
         return self._icon
+
     @property
     def unique_id(self):
         return self._object_id
@@ -150,8 +199,16 @@ class IHC8342BSensor(Entity):
 
     @property
     def device_state_attributes(self):
-        return {
-            ATTR_UPDATE_TIME: self._updateTime,
-            ATTR_DATA_TIME: self._dataTime,
-            ATTR_MAC_ADDR: self._mac
-        }
+        if self._type == "power":
+            return {
+                ATTR_UPDATE_TIME: self._updateTime,
+                ATTR_DATA_TIME: self._dataTime,
+                ATTR_MAC_ADDR: self._mac,
+            }
+        else:
+            return {
+                ATTR_UPDATE_TIME: self._updateTime,
+                ATTR_DATA_TIME: self._dataTime,
+                ATTR_MAC_ADDR: self._mac,
+                ATTR_PRICE: "%.2f元" % round(self._state * self._price, 2)
+            }
